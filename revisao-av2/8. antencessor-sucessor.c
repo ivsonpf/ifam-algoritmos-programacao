15427ce9bae46672db173f6975094ea02904748b
ivsonpf <154848332+ivsonpf@users.noreply.github.com>
2026-06-02 14:04:59 -0400

    Add program to calculate predecessor and successor

---
+++ revisao-av2/8. antencessor-sucessor.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Digite um numero: ");
+    scanf("%d", &n);
+    printf("Antecessor: %d, Sucessor: %d\n", n - 1, n + 1);
+    return 0;
+}
